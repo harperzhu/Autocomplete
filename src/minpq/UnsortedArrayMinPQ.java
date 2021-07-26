23872e80c5182b30202413fb39af7467c6642b17
harperzhu <harperzhu@yahoo.com>
2021-07-26 01:08:31 -0700

    First attempt with UnsortedArrayMinPQ

--- src/minpq/UnsortedArrayMinPQ.java
+++ src/minpq/UnsortedArrayMinPQ.java
@@ -15,45 +15,59 @@ public class UnsortedArrayMinPQ<T> implements ExtrinsicMinPQ<T> {
      * {@link List} of {@link PriorityNode} objects representing the item-priority pairs in no specific order.
      */
     private final List<PriorityNode<T>> items;
+    private PriorityNode<T> min;
 
     /**
      * Constructs an empty instance.
      */
     public UnsortedArrayMinPQ() {
         items = new ArrayList<>();
+
     }
 
     @Override
     public void add(T item, double priority) {
-        if (contains(item)) {
-            throw new IllegalArgumentException("Already contains " + item);
+        PriorityNode<T> new_item = new PriorityNode<>(item, priority);
+        if(new_item.priority()< min.priority()) {
+            min = new_item;
         }
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (!items.contains(new_item)) {
+                items.add(new_item);
+        }
     }
 
     @Override
     public boolean contains(T item) {
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        PriorityNode<T> temp = new PriorityNode<>(item, 0);
+        return items.contains(temp);
     }
 
     @Override
     public T peekMin() {
-        if (isEmpty()) {
+        if (items.isEmpty()) {
             throw new NoSuchElementException("PQ is empty");
         }
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        PriorityNode<T> min = items.get(0);
+        for(int i=0;i<items.size();i++){
+            if(items.get(i).priority() < min.priority()){
+                min = items.get(i);
+            }
+        }
+        return min.item();
     }
 
     @Override
     public T removeMin() {
-        if (isEmpty()) {
+        if (items.isEmpty()) {
             throw new NoSuchElementException("PQ is empty");
         }
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        for(int i=0; i<items.size();i++){
+            if(items.get(i).equals(min)){
+                items.get(i).equals(items.get(i+1));
+            }
+
+        }
+        return min.item();
     }
 
     @Override
@@ -61,13 +75,21 @@ public class UnsortedArrayMinPQ<T> implements ExtrinsicMinPQ<T> {
         if (!contains(item)) {
             throw new NoSuchElementException("PQ does not contain " + item);
         }
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        for(int i=0; i<items.size();i++){
+            if(items.get(i).equals(item)){
+                items.get(i).equals(null);
+                items.get(i).equals(new PriorityNode<>(item, priority));
+            }
+        }
     }
+
 
     @Override
     public int size() {
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        int size = 0;
+        for(int i=0; i<items.size();i++){
+            size += 1;
+        }
+        return size;
     }
 }
